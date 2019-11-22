cb001805257020dd0642ce44307d363bae490343
arita37 <noelkev0@gmail.com>
2019-11-23 00:06:17 +0900

    a

--- cli_code/cli_repo_check.py
+++ cli_code/cli_repo_check.py
@@ -1,64 +1,39 @@
 """
-
 Create python script to do this :
-
 For a given github repo :
-
    1)   git clone
    2)  Build conda env with packages
    3)  check if python code is running
             main.py
-  4) Generate signature docs of python source code
-
+   4) Generate signature docs of python source code
 
 Goal is to automate code check using a python script
-
 
 Example
 https://github.com/zhangqianhui/vae-gan-tensorflow
 
 
-"""
-
-
-
-
-#    1)   git clone repoURL
-
-
-
-
-
-
-# 2)  Build conda env from source code parsing
-  # cd repo
-#https://github.com/arita37/cli_code/blob/master/cli_code/cli_module_autoinstall.py
+#  1)   git clone repoURL
+#  2)  Build conda env from source code parsing
+   # cd repo
+# https://github.com/arita37/cli_code/blob/master/cli_code/cli_module_autoinstall.py
    
    
-   
-
 # 3) Launch all files in root (main.py) with subprocess to check if running
     # Log error messages on disk
- 
- 
- 
- 
- 
- 
  
 # 4) Parse source code with  import inspect and generate signature for each functiom
   #  Write on disk
    # https://github.com/arita37/cli_code/blob/master/cli_code/cli_module_analysis.py
 
-"""
 File: cll_build_check_repo.py
 Function: automate code check in repository
+
+
 """
 
-######################################
 
 ######################################
-
 import os
 import sys
 import errno
@@ -169,7 +144,7 @@ def repo_check_root_files(folder) :
   for file in file_list :
   	msg = os_system(["python ", file ])
     if "error" in msg :
-       pritn(msg)
+       print(msg)
 
 
 
