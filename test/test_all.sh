cedcb6a7253dc36e4257d081d64ab507c3d9b4db
raven <ravenredstain@outlook.com>
2021-01-25 17:55:18 +0500

    added one test for all utils, all tests are running successfully locally

--- test/test_all.sh
+++ test/test_all.sh
@@ -5,27 +5,27 @@ python   cli_code/cli_convert_ipynb.py   --in_folder  test/  --out_folder  test_
 
 
 # check cli_download by downloading from a link
-python   cli_code/cli_download -u https://drive.google.com/file/d/1FPn4Q4PClobHgEU4DglyF2Xbs5Boe1r_/view?usp=sharing -o test_out/
+python   cli_code/cli_download.py -u https://drive.google.com/file/d/1FPn4Q4PClobHgEU4DglyF2Xbs5Boe1r_/view?usp=sharing -o test_out/
 
 
 # this one requires git and conda installed on runner
-python   cli_code/cli_repo_check https://github.com/parul1931/walmart.git -n testing
+python   cli_code/cli_repo_check.py https://github.com/parul1931/walmart.git -n testing
 
 
 # check cli_conda_merge by joining two test conda env files
-python   cli_code/cli_conda_merge test/file0.yaml test/file1.yaml
+python   cli_code/cli_conda_merge.py test/file0.yml test/file1.yml
 
 
 # check cli_module_parser by parsing .py files in test dir
-python   cli_code/cli_module_parser test/ -o test_out/module_parsed.csv
+python   cli_code/cli_module_parser.py test/ -o test_out/module_parsed.csv
 
 
 # check cli_env_autoinstall by creating an environemnt for test repo
-python   cli_code/cli_env_autoinstall test/ -n notebook_cvt
+python   cli_code/cli_env_autoinstall.py test/ -n notebook_cvt
 
 
 # check cli_github_search by searching for some keywords
-python   cli_code/cli_github_search amazon scraper -o test_out/
+python   cli_code/cli_github_search.py amazon scraper -o test_out/
 
 # check cli_doc_auto by parsing .py files in test dir
 python cli_code/cli_doc_auto/main.py test/ -o test_out/docs.txt
