cbcb6fd1e30ff4e0187e7e0f67e3ecfcc313579b
Kev. Noel <arita37@users.noreply.github.com>
2021-05-28 00:03:40 +0900

    Update cmd.py

--- cli_code/sublime/googlesearch/cmd.py
+++ cli_code/sublime/googlesearch/cmd.py
@@ -1,9 +1,5 @@
 
-import os
-import webbrowser
-import sublime
-import sublime_plugin
-import sys
+import os, sys, webbrowser, sublime, sublime_plugin
 from .gitutil import GitUtil, main
 
 if sys.version_info < (3, 0):
@@ -12,44 +8,8 @@ else:
     from urllib.parse import quote_plus as quote_param
 
 
-def search(q, url=''):
-    settings = sublime.load_settings("google_search.sublime-settings")
-    # Attach the suffix and the prefix
-    q = settings.get('prefix', '') + quote_param(q) + settings.get('suffix', '')
 
-
-
-    if url == '':
-        fullUrl = settings.get('domain', 'https://www.google.com') + "/search?q={q}".format(q=q)
-
-
-    elif  url != '' : 
-        fullUrl = settings.get('domain', 'https://www.google.com') +  "/search?q={q}+sites%3A{url}".format(q=q, url=url)
-
-    else:
-        fullUrl = url + '/search?q=%s' % q
-    browser = settings.get('default_browser', '')
-
-
-    if browser:
-        try:
-            webbrowser.get(browser).open(fullUrl)
-        except:
-            webbrowser.open(fullUrl)
-    else:
-        webbrowser.open(fullUrl)
-  
-
-def git_command(path, command_string):
-    g = GitUtil(path)
-    # g.test()
-    print("adding files")
-    files = g.add()  # change the number to filter files on size , size in bytes
-    print('committing files')
-    g.commit(files)
-    print('done')
-
-
+#####################################################################################
 class TestCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         project_path = self.view.window().project_file_name()
@@ -73,27 +33,103 @@ class TestCommand(sublime_plugin.TextCommand):
             #print('query string is invalid')
             #return
 
+
+        #### Delete ##############################################
+        region = self.view.line(self.view.sel()[0])
+        self.view.erase(edit, r=region)
+
         for region in self.view.sel():
             self.view.erase(edit, r=region)
-            # self.view.line( self.view.sel()[0]).erase()
+
 
         ##########################################################
-        command        = query_list[0].lower().strip()
-        command_string = query_list[1].lower().strip()
-        print(command)
+        cmd        = query_list[0].lower().strip()
+        cmd_string = query_list[1].lower().strip()
+        print(cmd)
 
-        if command == 'git':
-            git_command(project_path, command_string)
+        if   cmd == 'git'    : do_git_command(cmd, project_path, cmd_string)
 
+        elif cmd == 'stack'  : do_search('google', cmd_string, 'stackoverflow.com')
+
+        elif cmd == 'github' : do_search('github', cmd_string, '')
+
+        elif cmd == 'go'     : do_gowebsite('go', cmd_string, '')
             
-        elif command == 'stack':
-            search(command_string, 'stackoverflow.com')
-
-            
-        else :  
-            ### Default Google
-            search(command_string, '')
+        else :                 do_search('google', cmd_string, '')    ### Default Google
 
 
 
+###############################################################################
+###### List of Actions  #######################################################
+def do_search(cmd='', q='', url=''):
+    settings = sublime.load_settings("google_search.sublime-settings")
+    # Attach the suffix and the prefix
+    q = settings.get('prefix', '') + quote_param(q) + settings.get('suffix', '')
+
+
+    if cmd == 'google':
+        if  url != '' : 
+            fullUrl = settings.get('domain', 'https://www.google.com') +  "/search?q={q}+sites%3A{url}".format(q=q, url=url)
+        else :
+            fullUrl = settings.get('domain', 'https://www.google.com') + "/search?q={q}".format(q=q)
+
+
+
+    else:
+        fullUrl = url + '/search?q=%s' % q
+    browser = settings.get('default_browser', '')
+
+
+    if browser:
+        try:
+            webbrowser.get(browser).open(fullUrl)
+        except:
+            webbrowser.open(fullUrl)
+    else:
+        webbrowser.open(fullUrl)
+  
+
+
+def do_git_command(path, command_string):
+    g = GitUtil(path)
+    # g.test()
+    print("adding files")
+    files = g.add()  # change the number to filter files on size , size in bytes
+    print('committing files')
+    g.commit(files)
+    print('done')
+
+
+
+def do_gowebsite(cmd='', q='', url=''):
+    settings = sublime.load_settings("google_search.sublime-settings")
+    # Attach the suffix and the prefix
+    # q = settings.get('prefix', '') + quote_param(q) + settings.get('suffix', '')
+    print(q)
+
+    if    'utilmy'   in q : fullUrl = 'https://github.com/arita37/myutil/tree/main/utilmy'
+    elif  'cli_code' in q : fullUrl = 'https://github.com/arita37/cli_code/tree/dev/cli_code/sublime/googlesearch'
+
+
+
+    else:
+        fullUrl = url + '/search?q=%s' % q
+
+
+    browser_open(fullUrl, settings)
+
+
+
+
+def browser_open(fullUrl, settings):
+    ### Open 
+    browser = settings.get('default_browser', '')
+    if browser:
+        try:
+            webbrowser.get(browser).open(fullUrl)
+        except:
+            webbrowser.open(fullUrl)
+    else:
+        webbrowser.open(fullUrl)
+  
 
